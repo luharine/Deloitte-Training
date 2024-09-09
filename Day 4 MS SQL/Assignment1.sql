4fda71d202d92dc8d591b39f9b3ab8d47eff0ff9
luharine <72065741+luharine@users.noreply.github.com>
2024-09-09 21:26:13 +0530

    Add files via upload

---
+++ Day 4 MS SQL/Assignment1.sql
@@ -0,0 +1,55 @@
+
+
+
+
+
+create table product(
+
+productId int primary key,
+productName varchar(10),
+price int,
+quantity int,
+category varchar(10)
+)
+
+
+Insert into product values(1,'name1',100,11,'catone');
+
+Insert into product values(2,'name2',1030,111,'cattwo');
+
+INSERT INTO product VALUES
+
+(3, 'name3', 1050, 120, 'catthree'),
+(4, 'name4', 1100, 130, 'catfour'),
+(5, 'name5', 1150, 140, 'catfive');
+
+
+
+
+
+INSERT INTO product VALUES
+(1, 'Laptop', 1500, 50, 'Electro'),
+(2, 'Smahone', 800, 200, 'Electr'),
+(3, 'Deshair', 120, 75, 'Furniture'),
+(4, 'Notebook', 5, 500, 'Statry'),
+(5, 'Water ', 15, 300, 'Accies'),
+(6, 'Headpnes', 200, 150, 'Elecics'),
+(7, 'Backpack', 60, 120, 'Accesies'),
+(8, 'Coffeker', 80, 40, 'Aances'),
+(9, 'RunShoes', 100, 90, 'Footwear'),
+(10, 'LED mp', 25, 60, 'Lighting');
+
+
+
+
+INSERT INTO product VALUES
+(1001, 'Laptop', 1500, 50, 'Elecs'),
+(1002, 'Smahone', 800, 200, 'Elics'),
+(1003, 'Deshair', 120, 75, 'Furnitu'),
+(1004, 'Notebook', 5, 500, 'Station'),
+(1005, 'Watere', 15, 300, 'Acces'),
+(1006, 'Headphones', 200, 150, 'Elonics'),
+(1007, 'Backpack', 60, 120, 'Accesies'),
+(1008, 'Co Maer', 80, 40, 'Applces'),
+(1009, 'Runnihoes', 100, 90, 'Footwear'),
+(1010, 'LED Lamp', 25, 60, 'Lighting');
