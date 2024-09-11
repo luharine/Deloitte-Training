d3bacf6122d73dfee52c477876251c5e7eb5aee5
luharine <72065741+luharine@users.noreply.github.com>
2024-09-11 09:38:47 +0530

    Add files via upload

---
+++ Day 7/Controllers/ProductController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+
+        ProductDbContext _context;
+        public ProductController(ProductDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProduct()
+        {
+            List<Product> Products = _context.Products.ToList();
+            return Ok(Products);
+
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+
+            Product obj = _context.Products.Find(id);
+            return Ok(obj);
+        }
+
+        [HttpPost]
+        public IActionResult PostProduct(Product obj)
+        {
+            if (obj != null)
+            {
+                _context.Products.Add(obj);
+                _context.SaveChanges();
+                return Ok(new { result = "Record inserted successfully" });
+            }
+            else
+            {
+
+                return NotFound(new { result = "Error in inserting the record" });
+            }
+        }
+
+        [HttpPut]
+        public IActionResult EditProduct(Product obj)
+        {
+
+
+            _context.Products.Update(obj);
+            _context.SaveChanges();
+
+            return Ok(new { result = "Updated Sucessfully" });
+
+        }
+
+
+
+
+    }
+}
---
+++ Day 7/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public double UnitPrice { get; set; }
+    }
+
+
+    public class ProductDbContext : DbContext
+    {
+        // This property refer the databse table 
+        // Multiple tables required multiple properties
+        public DbSet<Product> Products { get; set; }
+
+        public ProductDbContext(DbContextOptions<ProductDbContext> options)
+         : base(options)
+        {
+
+        }
+
+
+    }
+}
